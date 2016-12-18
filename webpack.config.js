d94602103e652b0e0b36677151b3f8d6a22d3bed
Colin But <colinbut@users.noreply.github.com>
2016-12-18 17:15:08 +0000

    uglify compress plugin to minify the js

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+const webpack = require('webpack');
+
 // webpack configuration file is commonjs-style module
 module.exports = {
   entry: './src/app.js',
@@ -11,5 +13,15 @@ module.exports = {
       exclude: /node_modules/, // avoid all external dependencies go through babel - slows down compilation
       loader: 'babel-loader'
     }]
-  }
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      },
+      output: {
+        comments: false
+      }
+    })
+  ]
 };
